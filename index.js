071426df45137d766be31b62dfa86b5a3d3bf1e4
DerekJ555 <joshuaderek99@gmail.com>
2026-07-23 13:48:46 -0700

    final bug fix

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const API_KEY = "AIzaSyAEWPx9Wf7X5gip6--4j8bCcUuxhm37tXM";
 //im sooooo tired :(
 
 async function fetchData(){
+    const searchQuery = document.getElementById("searchbar").value.toLowerCase();
     const resultsContainer = document.getElementById("resultsContainer");
     resultsContainer.innerHTML = `<p class="info-text">Searching for Videos...</p>`
     try{
 
-        const searchQuery = document.getElementById("searchbar").value.toLowerCase();
 
         const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${encodeURIComponent(searchQuery)}&type=video&key=${API_KEY}`);
 
@@ -48,24 +48,26 @@ async function fetchData(){
                 `;
             
             resultsContainer.appendChild(videoCard);
-            searchBooks(searchQuery);
-
             
-
+            
+            
             console.log(`Title:${item.snippet.title}`);
             console.log(`Video ID: ${item.id.videoId}`);
             console.log(`Thumbnail: ${item.snippet.thumbnails.default.url}`);
             console.log('-----------')
         });
-
-
+        
+        
         console.log(data);
-
+        
     }
-
+    
     catch(error){
         console.error(error);
         resultsContainer.innerHTML= `<p class="info-text">Failed to load Videos, Check connection or Change Browser and Try Again</p>`
+    }
+    finally{
+        searchBooks(searchQuery);
     }
 }
 
@@ -73,7 +75,7 @@ async function fetchData(){
 
 async function searchBooks(searchQuery){
     const booksContainer = document.getElementById("booksContainer")
-    booksContainer.innerHTML = "<p>Searching for books</p>"
+    booksContainer.innerHTML = `<p class="info-text">Searching for books</p>`
     try{
 
         const response = await fetch(`https://openlibrary.org/search.json?q=${encodeURIComponent(searchQuery)}&limit=50`);
@@ -81,8 +83,8 @@ async function searchBooks(searchQuery){
 
 
         booksContainer.innerHTML="";
-        if(!booksContainer.docs || booksContainer.docs.length===0){
-            booksContainer.innerHTML =" <p>Books not Found</p>"
+        if(!bookdata.docs || bookdata.docs.length===0){
+            booksContainer.innerHTML =`<p class="info-text">Books not Found</p>`
             return;
         }
 
@@ -115,7 +117,7 @@ async function searchBooks(searchQuery){
 
     catch(error){
         console.error(error);
-        booksContainer.innerHTML = "<p>Failed to load books, check internet connection or change browser and try again</p>"
+        booksContainer.innerHTML = `<p class="info-text">Failed to load books, check internet connection or change browser and try again</p>`
     }
 }
 
